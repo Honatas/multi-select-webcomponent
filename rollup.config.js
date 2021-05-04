e2f653551a92f03a4560ae7090a4eceadb659f19
Jonatas <jmoraes@gmail.com>
2021-05-04 16:21:51 -0300

    package files information

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/multi-select-webcomponent.ts',
@@ -12,11 +13,15 @@ export default {
     file: 'dist/multi-select-webcomponent.js',
     format: 'umd',
     name: 'MultiselectWebcomponent'
+  }, {
+    file: 'dist/multi-select-webcomponent.min.js',
+    format: 'umd',
+    name: 'MultiselectWebcomponent',
+    plugins: [
+      terser()
+    ]
   }],
   plugins: [
-    typescript({
-      declaration: true,
-      declarationDir: 'dist'
-    })
+    typescript()
   ]
 }
